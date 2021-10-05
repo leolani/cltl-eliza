aff9f79c2e9bb8946c54155edb8735e5906c723c
Thomas Baier <tbaier@math.bme.hu>
2021-10-05 14:31:58 +0200

    Add minimal build setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("VERSION", "r") as fh:
+    version = fh.read().strip()
+
+setup(
+    name='cltl.eliza',
+    version=version,
+    packages=find_namespace_packages(include=['cltl.*']),
+    data_files=[('VERSION', ['VERSION'])],
+    url="https://github.com/leolani/cltl-eliza",
+    license='MIT License',
+    author='CLTL',
+    author_email='t.baier@vu.nl',
+    description='Eliza component for Leolani',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+    install_requires=[],
+)
