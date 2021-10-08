9f8ad5961d399a52b13de2fef2b7cf99533ba235
Thomas Baier <tbaier@math.bme.hu>
2021-10-08 11:53:07 +0200

    Add Eliza service and tests

---
+++ src/cltl_service/eliza/service.py
@@ -0,0 +1,69 @@
+import logging
+import uuid
+
+import time
+from cltl.combot.infra.config import ConfigurationManager
+from cltl.combot.infra.event import Event, EventBus
+from cltl.combot.infra.resource import ResourceManager
+from cltl.combot.infra.topic_worker import TopicWorker
+from cltl_service.backend.schema import TextSignalEvent
+
+from cltl.eliza.api import Eliza
+
+logger = logging.getLogger(__name__)
+
+
+CONTENT_TYPE_SEPARATOR = ';'
+
+
+class ElizaService:
+    @classmethod
+    def from_config(cls, eliza: Eliza, event_bus: EventBus, resource_manager: ResourceManager,
+                    config_manager: ConfigurationManager):
+        config = config_manager.get_config("cltl.eliza")
+
+        return cls(config.get("topic_input"), config.get("topic_output"), eliza, event_bus, resource_manager)
+
+    def __init__(self, input_topic: str, output_topic: str, eliza: Eliza,
+                 event_bus: EventBus, resource_manager: ResourceManager):
+        self._eliza = eliza
+
+        self._event_bus = event_bus
+        self._resource_manager = resource_manager
+
+        self._input_topic = input_topic
+        self._output_topic = output_topic
+
+        self._topic_worker = None
+
+    @property
+    def app(self):
+        return None
+
+    def start(self, timeout=30):
+        self._topic_worker = TopicWorker([self._input_topic], self._event_bus, provides=[self._output_topic],
+                                         resource_manager=self._resource_manager, processor=self._process)
+        self._topic_worker.start().wait()
+
+        greeting_payload = self._create_payload(self._eliza.respond(None))
+        self._event_bus.publish(self._output_topic, Event.for_payload(greeting_payload))
+
+    def stop(self):
+        if not self._topic_worker:
+            pass
+
+        self._topic_worker.stop()
+        self._topic_worker.await_stop()
+        self._topic_worker = None
+
+    def _process(self, event: Event[TextSignalEvent]):
+        payload = event.payload
+        response = self._eliza.respond(payload.text)
+
+        eliza_event = self._create_payload(response)
+        self._event_bus.publish(self._output_topic, Event.for_payload(eliza_event))
+
+    def _create_payload(self, response):
+        signal_id = str(uuid.uuid4())
+
+        return TextSignalEvent.create(signal_id, time.time(), response, [])
