8c7ee6311d7a845ef4637df0192b13438abc4e09
Thomas Baier <tbaier@math.bme.hu>
2022-05-31 14:31:36 +0200

    Remove unused dependency

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.7',
-    install_requires=['cltl.combot', 'cltl.backend', 'emissor'],
+    install_requires=['cltl.combot', 'emissor'],
 )
