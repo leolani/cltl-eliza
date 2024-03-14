ebb7195c6595f1c0e9c45b316e38870884161254
piek <piek.vossen@vu.nl>
2024-03-14 16:13:44 +0100

    Update eliza.py
    Making language sensitive and adding Dutch

--- src/cltl/eliza/eliza.py
+++ src/cltl/eliza/eliza.py
@@ -7,7 +7,7 @@ from cltl.eliza.api import Eliza
 
 NAME = "Eliza"
 GREETING = f"Hello. I am {NAME}. I am your personal therapist and your best friend. How are you feeling today?"
-
+LANG = "EN"
 
 class ElizaImpl(Eliza):
     def __init__(self):
@@ -27,13 +27,24 @@ class ElizaImpl(Eliza):
     def _reflect(self, fragment):
         tokens = fragment.lower().split()
         for i, token in enumerate(tokens):
-            if token in lang.REFLECTIONS:
-                tokens[i] = lang.REFLECTIONS[token]
+            if LANG=="NL":
+                if token in lang.REFLECTIONS_NL:
+                    tokens[i] = lang.REFLECTIONS_NL[token]
+            else:
+                if token in lang.REFLECTIONS:
+                    tokens[i] = lang.REFLECTIONS[token]
         return ' '.join(tokens)
 
     def _analyze(self, statement):
-        for pattern, responses in lang.PSYCHOBABBLE:
-            match = re.match(pattern, statement.rstrip(".!"))
-            if match:
-                response = random.choice(responses)
-                return response.format(*[self._reflect(g) for g in match.groups()])
+        if LANG=="NL":
+            for pattern, responses in lang.PSYCHOBABBLE_NL:
+                match = re.match(pattern, statement.rstrip(".!").lower())
+                if match:
+                    response = random.choice(responses)
+                    return response.format(*[self._reflect(g) for g in match.groups()])
+        else:
+            for pattern, responses in lang.PSYCHOBABBLE:
+                match = re.match(pattern, statement.rstrip(".!").lower())
+                if match:
+                    response = random.choice(responses)
+                    return response.format(*[self._reflect(g) for g in match.groups()])
